59fb88b3d9e1a45020f3aae01bd0375acd23074d
rafalkaron <rafalkaron@gmail.com>
2025-09-06 14:57:01 +0200

    Update test

--- tests/test_buttons.py
+++ tests/test_buttons.py
@@ -9,20 +9,45 @@ KEY3_PIN = 2
 
 
 def on_key1():
-    print("[TEST] KEY1 callback triggered!")
+    print("[TEST] KEY1 short-press callback triggered!")
+
+
+def on_key1_long():
+    print("[TEST] KEY1 long-press callback triggered!")
 
 
 def on_key2():
-    print("[TEST] KEY2 callback triggered!")
+    print("[TEST] KEY2 short-press callback triggered!")
+
+
+def on_key2_long():
+    print("[TEST] KEY2 long-press callback triggered!")
 
 
 def on_key3():
-    print("[TEST] KEY3 callback triggered!")
+    print("[TEST] KEY3 short-press callback triggered!")
+
+
+def on_key3_long():
+    print("[TEST] KEY3 long-press callback triggered!")
 
 
 def main():
-    print("[TEST] Press each button to test callbacks. Press Ctrl+C to exit.")
-    Buttons(KEY1_PIN, KEY2_PIN, KEY3_PIN, on_key1, on_key2, on_key3)
+    print(
+        "[TEST] Press each button to test short and long-press callbacks. Press Ctrl+C to exit."
+    )
+    Buttons(
+        KEY1_PIN,
+        KEY2_PIN,
+        KEY3_PIN,
+        on_key1,
+        on_key2,
+        on_key3,
+        on_key1_long,
+        on_key2_long,
+        on_key3_long,
+        long_press_ms=5000,
+    )
     try:
         while True:
             utime.sleep(1)
